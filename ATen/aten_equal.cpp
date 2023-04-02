1b6510a1e8d98ec04387681abdfcd346e9f2f88e
Ryan Moore <ryanm.inbox@gmail.com>
2023-04-02 02:21:59 -0400

    Aten equality update CMakelists nightmare and run.sh

---
+++ ATen/aten_equal.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <ATen/ATen.h>
+
+int main() {
+  // create two tensors
+  at::Tensor a = at::ones({2, 2}, at::kInt);
+  at::Tensor b = at::ones({2, 2}, at::kInt);
+
+  // element-wise comparison
+  at::Tensor elementwise_comparison = a.eq(b);
+  std::cout << "Element-wise comparison:\n" << elementwise_comparison << std::endl;
+
+  // whole tensor comparison
+  bool whole_tensor_comparison = at::equal(a, b);
+  std::cout << "Whole tensor comparison: " << std::boolalpha << whole_tensor_comparison << std::endl;
+
+  return 0;
+}
--- ATen/run.sh
+++ ATen/run.sh
@@ -1,5 +1,7 @@
+rm -rf build
 mkdir build
 cd build
 cmake -DCMAKE_PREFIX_PATH=$(python -c 'import torch;print(torch.utils.cmake_prefix_path)') ..
 cmake --build .
 ./aten_example
+./aten_equal
