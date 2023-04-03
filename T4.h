aebae3827b84685050728dd857f55f3355a622e4
Ryan Moore <ryanm.inbox@gmail.com>
2023-04-02 23:15:33 -0400

    "Refactor Tensor class to introduce Storage and support views on data"
    - Introduced a separate Storage class that handles the underlying data storage. The Tensor class now has a shared pointer to a Storage instance, allowing for multiple tensors to share the same storage when they represent views on the same data.
    - Added an offset_ member to the Tensor class to represent the starting position of the logical tensor within the storage. This allows for creating views on the storage without copying data.
    - Modified the Tensor's operator() to handle indexing based on the strides and the offset. This allows for convenient access to elements in the tensor using a vector of indices.

--- T4.h
+++ T4.h
@@ -4,34 +4,58 @@
 #ifndef TENSOR_H_
 #define TENSOR_H_
 
+#include <initializer_list>
 #include <memory>
 #include <stdexcept>
 #include <vector>
 
 template <typename T>
+class Storage {
+ public:
+  explicit Storage(size_t size) : data_(size) {}
+  T &operator[](size_t idx) { return data_[idx]; }
+  const T &operator[](size_t idx) const { return data_[idx]; }
+
+ private:
+  std::vector<T> data_;
+};
+
+template <typename T>
 class Tensor {
  public:
-  Tensor(std::vector<size_t> dimensions, std::vector<T> values = {})
-      : sizes_(std::move(dimensions)), data_(std::move(values)) {
-    size_t total_size = 1;
-    for (const auto &dim : sizes_) {
-        total_size *= dim;
-    }
-    if (data_.empty()) {
-      data_.resize(total_size);
-    } else {
-      if (data_.size() != expected_size) {
-        throw std::runtime_error(
-            "Size of the provided values vector does not match the product of "
-            "dimensions.");
+  Tensor(std::initializer_list<size_t> dimensions, std::vector<T> values = {})
+      : sizes_(dimensions),
+        storage_(std::make_shared<Storage<T>>(values.empty() ? computeSize()
+                                                             : values.size())),
+        offset_(0),
+        data_(std::move(values)) {
+    if (!data_.empty()) {
+      for (size_t i = 0; i < data_.size(); ++i) {
+        (*storage_)[i] = data_[i];
       }
     }
     computeStrides();
   }
 
+  size_t computeSize() const {
+    size_t total_size = 1;
+    for (const auto &dim : sizes_) {
+      total_size *= dim;
+    }
+    return total_size;
+  }
+
+  T &operator()(const std::vector<size_t> &indices) {
+    size_t idx = offset_;
+    for (size_t i = 0; i < indices.size(); ++i) {
+      idx += indices[i] * strides_[i];
+    }
+    return (*storage_)[idx];
+  }
+
   const std::vector<size_t> &sizes() const { return sizes_; }
-  const std::vector<T> &data() const { return data_; }
   const std::vector<size_t> &strides() const { return strides_; }
+  const std::shared_ptr<Storage<T>> &storage() const { return storage_; }
 
  private:
   void computeStrides() {
@@ -43,8 +67,9 @@ class Tensor {
     }
   }
 
-  // typedef dtype T
   std::vector<size_t> sizes_;
+  std::shared_ptr<Storage<T>> storage_;
+  size_t offset_;
   std::vector<T> data_;
   std::vector<size_t> strides_;
 };
