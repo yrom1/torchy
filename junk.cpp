c7b9045dd0c0dc93df098cb7622be805e3755773
Ryan Moore <ryanm.inbox@gmail.com>
2023-04-07 15:47:26 -0400

    matmul is working but give more detail on the + b error

--- junk.cpp
+++ junk.cpp
@@ -6,9 +6,17 @@ int main() {
   Tensor<int> x({3, 2}, {1, 2, 3, 4, 5, 6});
   Tensor<int> w({2, 4}, {1, 2, 3, 4, 5, 6, 7, 8});
   Tensor<int> b({1, 4}, {1, 2, 3, 4});
-  Tensor<int> result = x.matmul(w) + b;
-  // BUG broadcasting we need to broadcast O_O!!!
-  /*
+  // BUG broadcasting we need to broadcast O_O
+  //     broken because of + b!
+  // Does not work:
+  // Tensor<int> result = x.matmul(w) + b;
+  // Works:
+  Tensor<int> result = x.matmul(w);
+  std::cout << x << std::endl;
+  std::cout << w << std::endl;
+  std::cout << b << std::endl;
+  std::cout << result << std::endl;
+  /* this is without the + b
   [[1, 2],
   [3, 4],
   [5, 6]]
@@ -19,11 +27,20 @@ int main() {
   [23, 30, 37, 44],
   [35, 46, 57, 68]]
   */
-
-  Tensor<int> result = x.matmul(w) + b;
-  std::cout << x << std::endl;
-  std::cout << w << std::endl;
-  std::cout << b << std::endl;
-  std::cout << result << std::endl;
+  /*
+  >>> x = torch.tensor(((1, 2), (3, 4), (5, 6)))
+  >>> w = torch.tensor(((1, 2, 3, 4), (5, 6, 7, 8)))
+  >>> x
+  tensor([[1, 2],
+          [3, 4],
+          [5, 6]])
+  >>> w
+  tensor([[1, 2, 3, 4],
+          [5, 6, 7, 8]])
+  >>> x.matmul(w)
+  tensor([[11, 14, 17, 20],
+          [23, 30, 37, 44],
+          [35, 46, 57, 68]])
+  */
   auto result_v = result.storage()->data();
 }
