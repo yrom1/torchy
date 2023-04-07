71a44518f5387af1c3ae9bce21fef68b7b1e26b1
Ryan Moore <ryanm.inbox@gmail.com>
2023-04-07 10:05:04 -0400

    get neuron data into vector

--- ATen/aten_neuron.cpp
+++ ATen/aten_neuron.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <ATen/ATen.h>
+#include <vector>
 
 int main() {
   std::vector<int> data1 = {1, 2, 3, 4, 5, 6};
@@ -14,6 +15,17 @@ int main() {
 
   std::cout << "NEURON" << std::endl;
   std::cout << result_aten << std::endl;
+
+  // Get the data out of result_aten and put it in a vector
+  std::vector<int> result_vector(result_aten.numel());
+  std::memcpy(result_vector.data(), result_aten.data_ptr<int>(), result_aten.numel() * sizeof(int));
+
+  // Print the result vector
+  std::cout << "Result vector:" << std::endl;
+  for (int value : result_vector) {
+    std::cout << value << " ";
+  }
+  std::cout << std::endl;
 
   return 0;
 }
