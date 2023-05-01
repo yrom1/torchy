ca1f7c805a572bef43d965e2715dd1813410555f
Ryan Moore <ryanm.inbox@gmail.com>
2023-05-01 01:49:29 -0400

    add  c = (a * b) - ((b / a) + b) test failing

--- test.cpp
+++ test.cpp
@@ -300,6 +300,25 @@ TEST(Torch, DivBackward0Scalar) {
   EXPECT_EQ(b_v, b_v_t);
 }
 
+TEST(Torch, ScalarMultiOperator) {
+  Tensor<float> a({1}, {4.20}, true);
+  Tensor<float> b({1}, {13.37}, true);
+  auto c = (a * b) - ((b / a) + b);
+  c.backward();
+  auto a_v = a.autograd_meta_.get()->grad_.storage_.get()->data_;
+  auto b_v = b.autograd_meta_.get()->grad_.storage_.get()->data_;
+
+  torch::Tensor a_t = torch::tensor({4.20}, torch::requires_grad(true));
+  torch::Tensor b_t = torch::tensor({13.37}, torch::requires_grad(true));
+  torch::Tensor c_t = (a_t * b_t) - ((b_t / a_t) + b_t);
+  c_t.backward();
+  auto a_v_t = tensorToVector<float>(a_t.grad());
+  auto b_v_t = tensorToVector<float>(b_t.grad());
+
+  EXPECT_EQ(a_v, a_v_t);
+  EXPECT_EQ(b_v, b_v_t);
+}
+
 TEST(Torch, Neuron) {
   // Test input tensors
   Tensor<int> x({3, 2}, {1, 2, 3, 4, 5, 6});
