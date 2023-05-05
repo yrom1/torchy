7de3c5830e22d230141ac752d0218c980ea9ea47
Ryan Moore <ryanm.inbox@gmail.com>
2023-05-05 01:26:13 -0400

    shared_from_this add.cpp

--- add.cpp
+++ add.cpp
@@ -4,44 +4,51 @@
 
 class Foo : public std::enable_shared_from_this<Foo> {
 public:
-  int value_;
-  std::vector<std::shared_ptr<Foo>> children_;
+    int value_;
+    std::vector<std::shared_ptr<Foo>> children_;
 
-  Foo(int value = 0) : value_(value) {}
+    Foo(int value = 0) : value_(value) {}
 
-  std::shared_ptr<Foo> binaryAdd(const std::shared_ptr<Foo>& other) {
-    children_.push_back(this->shared_from_this());
-    children_.push_back(other);
-    return std::make_shared<Foo>(this->value_ + other->value_);
-  }
-
-  std::shared_ptr<Foo> operator+(const std::shared_ptr<Foo>& other) {
-    return binaryAdd(other);
-  }
-
-  std::shared_ptr<Foo> operator+(int value) {
-    return binaryAdd(std::make_shared<Foo>(value));
-  }
-
-  void children() {
-    for (const auto & x : children_) {
-      std::cout << x << std::endl; // Print the value of each child
+    std::shared_ptr<Foo> binaryAdd(const std::shared_ptr<Foo>& other) {
+        children_.push_back(other);
+        return std::make_shared<Foo>(value_ + other->value_);
     }
-  }
+
+    std::shared_ptr<Foo> operator+(const std::shared_ptr<Foo>& other) {
+        return binaryAdd(other);
+    }
+
+    std::shared_ptr<Foo> operator+(int value) {
+        Foo tmp(value);
+        return binaryAdd(std::make_shared<Foo>(tmp));
+    }
+
+    std::shared_ptr<Foo> operator+(const Foo& other) {
+        return *this + std::make_shared<Foo>(other);
+    }
+
+    void children() {
+        for (const auto& x : children_) {
+            std::cout << x->value_ << std::endl;
+        }
+    }
 };
 
 int main() {
-  auto foo1 = std::make_shared<Foo>(5);
-  auto foo2 = std::make_shared<Foo>(7);
-  auto foo3 = std::make_shared<Foo>(4);
+    Foo foo1(5);
+    Foo foo2(7);
+    Foo foo3(4);
 
-  auto result = *foo1 + 3;
-  auto resultc = *foo1 + foo3;
+    auto result = foo1 + 3;
+    auto resultc = foo1 + foo3;
+    auto resulti = foo1 + Foo(3);
 
-  std::cout << "Foo(5) + 3" << std::endl;
-  std::cout << result->value_ << std::endl;
-  std::cout << "Foo(5) + Foo(4)" << std::endl;
-  std::cout << resultc->value_ << std::endl;
-  std::cout << "children" << std::endl;
-  foo1->children();
+    std::cout << "Foo(5) + 3" << std::endl;
+    std::cout << result->value_ << std::endl;
+    std::cout << "Foo(5) + Foo(4)" << std::endl;
+    std::cout << resultc->value_ << std::endl;
+    std::cout << "Foo(5) + Foo(3)" << std::endl;
+    std::cout << resulti->value_ << std::endl;
+    std::cout << "children" << std::endl;
+    foo1.children();
 }
