3d33d6ddda5d2095e76c50ac6ac3db06cd7aa06b
Ryan Moore <ryanm.inbox@gmail.com>
2023-05-06 06:47:50 -0400

    working shared pointer Tensor operator+ children:
    ```
    (main) Ryans-Air:torchy ryan$ sh cling.sh
    [cling]$ ag::T a = ag::tensor({1}, {42.0});
    [cling]$ ag::T b = ag::tensor({1}, {42.0});
    [cling]$ a
    (ag::T &) std::shared_ptr -> 0x6000024133d8
    [cling]$ b
    (ag::T &) std::shared_ptr -> 0x600002404888
    [cling]$ auto c = a + b
    (std::shared_ptr<ag::Tensor> &) std::shared_ptr -> 0x600002404d98
    [cling]$
    [cling]$ c
    (std::shared_ptr<ag::Tensor> &) std::shared_ptr -> 0x600002404d98
    [cling]$ c.get()->children()
    0x6000024133d8
    0x600002404888
    [cling]$ a
    (ag::T &) std::shared_ptr -> 0x6000024133d8
    [cling]$ b
    (ag::T &) std::shared_ptr -> 0x600002404888
    ```

--- torchy.hpp
+++ torchy.hpp
@@ -14,6 +14,7 @@
 #include <iostream>
 #include <memory>
 #include <vector>
+#include <cassert>
 
 namespace ag {
 
@@ -26,14 +27,16 @@ public:
     char op_;
 
     Tensor(std::initializer_list<int> size, std::initializer_list<float> data) : size_(size), data_(data), grad_(_product(size), 0), children_(), op_('?') {};
-    Tensor(std::vector<int> size, std::vector<float> data) : size_(size), data_(data), grad_(_product(size), 0), children_(), op_('?') {};
+    Tensor(std::vector<int> size, std::vector<float> data, std::vector<std::shared_ptr<Tensor>> children) : size_(size), data_(data), grad_(_product(size), 0), children_(children), op_('?') {};
 
     std::shared_ptr<Tensor> get_shared() {
         return this->shared_from_this();
     }
 
+    void size() { for (auto x : size_) std::cout << x << std::endl; }
     void data() { for (auto x : data_) std::cout << x << std::endl; }
     void grad() { for (auto x : grad_) std::cout << x << std::endl; }
+    void children() { for (auto x : children_) std::cout << x << std::endl; }
 
 private:
     int _product(std::vector<int> size) {
@@ -45,13 +48,27 @@ private:
     }
 };
 
-std::shared_ptr<Tensor> tensor(std::initializer_list<int> size, std::initializer_list<float> data) {
+std::shared_ptr<Tensor> operator+(std::shared_ptr<Tensor> lhs, std::shared_ptr<Tensor> rhs) {
+    std::vector<std::shared_ptr<Tensor>> children;
+    children.push_back(lhs);
+    children.push_back(rhs);
+
+    assert(lhs.get()->data_.size() == rhs.get()->data_.size());
+    std::vector<float> result_data(lhs.get()->data_.size());
+    for (size_t i = 0; i < lhs.get()->data_.size(); ++i) {
+        result_data[i] = lhs.get()->data_[i] + rhs.get()->data_[i];
+    }
+    auto result = std::make_shared<Tensor>(lhs.get()->size_, result_data, children);
+    return result;
+}
+
+std::shared_ptr<Tensor> tensor(std::initializer_list<int> size, std::initializer_list<float> data, std::vector<std::shared_ptr<Tensor>> children = {}) {
     Tensor t(size, data);
     return std::make_shared<Tensor>(t);
 }
 
-std::shared_ptr<Tensor> tensor(std::vector<int> size, std::vector<float> data) {
-    Tensor t(size, data);
+std::shared_ptr<Tensor> tensor(std::vector<int> size, std::vector<float> data, std::vector<std::shared_ptr<Tensor>> children = {}) {
+    Tensor t(size, data, {});
     return std::make_shared<Tensor>(t);
 }
 
