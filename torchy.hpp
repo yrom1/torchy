00d15b99c1cc152bee9b77516605181eba3b5ab2
Ryan Moore <ryanm.inbox@gmail.com>
2023-05-06 05:30:55 -0400

    ag::tensor() working

--- torchy.hpp
+++ torchy.hpp
@@ -1,29 +1,47 @@
 // Copyright 2023 Ryan Moore
+//
+// "If you remember just one thing out of this course, it should be this – two
+// pieces of advice. The first piece of advice is whenever you can, use vector.
+// The second piece of advice is if you cannot, find a way how you can. This is
+// to avoid any data structures except arrays... Vector is an array, right. Sort
+// of, you say, 'Well... Aren't there exceptions?' – Yes, not for you!"
+//
+// — Alexander Stepanov
 
 #ifndef TORCHY_HPP_
 #define TORCHY_HPP_
 
 #include <iostream>
 #include <memory>
+#include <vector>
 
 namespace ag {
 
 class Tensor : public std::enable_shared_from_this<Tensor> {
 public:
-    float value_;
+    std::vector<size_t> size_;
+    std::vector<float> data_;
+    std::vector<float> grad_;
+    std::vector<std::shared_ptr<Tensor>> children_;
+    char op_;
 
-    Tensor(float value = 0) : value_(value) {}
+    Tensor(std::initializer_list<size_t> size, std::initializer_list<float> data) : size_(size), data_(data), grad_(), children_(), op_() {};
+    Tensor(std::vector<size_t> size, std::vector<float> data) : size_(size), data_(data), grad_(), children_(), op_() {};
 
     std::shared_ptr<Tensor> get_shared() {
         return this->shared_from_this();
     }
+
+    void data() { for (auto x : data_) std::cout << x << std::endl; }
+    void grad() { for (auto x : grad_) std::cout << x << std::endl; }
 };
 
-using T = std::shared_ptr<Tensor>;
-
-T tensor(float value) {
-    return std::make_shared<Tensor>(value);
+std::shared_ptr<Tensor> tensor() {
+    Tensor t({1}, {42.0});
+    return std::make_shared<Tensor>(t);
 }
+
+using T = std::shared_ptr<Tensor>;
 
 }
 
