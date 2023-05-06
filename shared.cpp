c9239d5b3a3caf93d974c733c6825177648c283d
Ryan Moore <ryanm.inbox@gmail.com>
2023-05-06 03:41:27 -0400

    ag::T foo = ag::tensor(42);

--- shared.cpp
+++ shared.cpp
@@ -5,30 +5,26 @@ namespace ag {
 
 class Tensor : public std::enable_shared_from_this<Tensor> {
 public:
-    int value_;
+    float value_;
 
-    using dtype = std::shared_ptr<Tensor>;
+    Tensor(float value = 0) : value_(value) {}
 
-    Tensor(int value = 0) : value_(value) {}
-
-    std::shared_ptr<Tensor> getSharedFromThis() {
-        return shared_from_this();
+    std::shared_ptr<Tensor> get_shared() {
+        return this->shared_from_this();
     }
 };
 
 using T = std::shared_ptr<Tensor>;
 
+T tensor(float value) {
+    return std::make_shared<Tensor>(value);
+}
+
 }
 
 int main() {
-    ag::T foo = std::make_shared<ag::Tensor>(42);
-    ag::T bar = foo->getSharedFromThis();
+    ag::T foo = ag::tensor(42);
+    ag::T bar = foo->get_shared();
     std::cout << foo->value_ << " " << foo << std::endl;
     std::cout << bar->value_ << " " << bar << std::endl;
 }
-
-/*
-(main) Ryans-Air:torchy ryan$ c++ -std=c++20 shared.cpp && ./a.out
-42 0x60000312c258
-42 0x60000312c258
-*/
