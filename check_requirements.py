d640c91e391dd8c666f63f6221c21f94d45d4b08
Ryan Moore <ryanm.inbox@gmail.com>
2023-07-09 16:18:33 -0400

    wip check requirements, (to make sure desktop is setup correctly)

---
+++ check_requirements.py
@@ -0,0 +1,20 @@
+import shutil
+
+
+def check_program(program):
+    """Checks if the specified program is installed"""
+    path = shutil.which(program)
+    if path is None:
+        print(f"{program} is not installed.")
+    else:
+        print(f"{program} is installed at {path}.")
+
+
+def main():
+    programs = ["clang-format", "cpplint", "cmake"]
+    for program in programs:
+        check_program(program)
+
+
+if __name__ == "__main__":
+    main()
